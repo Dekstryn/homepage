6ddcfce6554cc74dbeace923cd2ae475c536a803
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-06-06 05:59:47 +0200

    Adjusted text message in main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const name = "Krzysiek";
 const age = "35";
 console.log(name);
 console.log(age);
-console.log(`Nazywa się ${name}	i mam ${age} lat`);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
