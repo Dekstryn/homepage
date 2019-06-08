97453fdc70312754fb61394df13ee37a92a5f63b
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-06-08 06:39:24 +0200

    Added few log to main.js

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ const age = "35";
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('Uczę się tworzyć strony html.');
+console.log('Fajnie, że tu zaglądasz :)');
+console/log('Życzę Ci Miłego dnia.');
