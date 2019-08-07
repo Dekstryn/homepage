90b748cdeb88c278fc65f34200888c0fdbe1f6d9
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-07 17:59:42 +0200

    Added padding modification to mheader__h1

--- js/main.js
+++ js/main.js
@@ -21,4 +21,6 @@ const navSwitcher = document.querySelector('.nav__switcher--js')
 navSwitcher.addEventListener('click', (e) =>{
   const navigationList = document.querySelector('.portal-navigation--js');
   navigationList.classList.toggle('portal-navigation--visible');
+  const navigationList = document.querySelector('.mheader__h1--js');
+  navigationList.classList.toggle('mheader__h1--padding');
 });
