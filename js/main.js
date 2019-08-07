bd49d06ad2944d3542d30a2071be4980d3fd8ba9
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-07 19:34:24 +0200

    Deleted unnessesery code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const name = "Krzysiek";
-const age = "35";
-console.log('Cześć,');
-console.log(`nazywam się ${name} i mam ${age} lat.`);
-console.log('Uczę się tworzyć strony html.');
-console.log('Fajnie, że tu zaglądasz :)');
-console.log('Życzę Ci Miłego dnia.');
-
-const paragraph = document.querySelector('.msection__p--js');
-paragraph.innerHTML = 'To jest akapit testowy :)'
-function your_name(name, age){
-  if (age >=18){
-  console.log('Cześć ' + name + ' jesteś pełnoletni, więc możesz wejść na stronę')
-  }
-  else{
-    console.log('Przykro mi ' + name + ' nie jesteś pełnoletni, więc nie możesz wejść na stronę')
-  }
-}
 const navSwitcher = document.querySelector('.nav__switcher--js')
 
 navSwitcher.addEventListener('click', (e) =>{
