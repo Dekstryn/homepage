36bea8c1a965c88c06c402ae5d5c5ff64d9cadb1
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-07 18:02:22 +0200

    Repeired addEventListener function

--- js/main.js
+++ js/main.js
@@ -21,6 +21,8 @@ const navSwitcher = document.querySelector('.nav__switcher--js')
 navSwitcher.addEventListener('click', (e) =>{
   const navigationList = document.querySelector('.portal-navigation--js');
   navigationList.classList.toggle('portal-navigation--visible');
-  const navigationList = document.querySelector('.mheader__h1--js');
-  navigationList.classList.toggle('mheader__h1--padding');
+});
+navSwitcher.addEventListener('click', (e) =>{
+const navigationList = document.querySelector('.mheader__h1--js');
+navigationList.classList.toggle('mheader__h1--padding');
 });
