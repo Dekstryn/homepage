bcfdb45ff7d2cb849674cc294248258ba92a7513
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-08 05:22:42 +0200

    Changed modification class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const navSwitcher = document.querySelector('.nav__switcher--js')
 
 navSwitcher.addEventListener('click', (e) =>{
-  const navigationList = document.querySelector('.portal-navigation--js');
-  navigationList.classList.toggle('portal-navigation--visible');
+  const navigationList = document.querySelector('.nav__text--js');
+  navigationList.classList.toggle('nav__text--visible');
 });
 navSwitcher.addEventListener('click', (e) =>{
 const navigationList = document.querySelector('.mheader__h1--js');
